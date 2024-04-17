c7fa7536a68c25a412dadd59816048ed39d982c5
Vivek Awasthi <vivekawasthi055@gmail.com>
2024-04-17 17:22:13 +0530

    updated

--- src/components/TermsAndConditions/TermsAndConditions.jsx
+++ src/components/TermsAndConditions/TermsAndConditions.jsx
@@ -5,13 +5,13 @@ const TermsAndConditions = () => {
   return (
     <section id="termsandcond">
       <header>
-        <p>CURRENT AS OF MARCH 2024</p>
+        <p>CURRENT AS OF MAY 2024</p>
         <h1>Terms & Conditions</h1>
       </header>
       <div className="content">
         <p>
-          Welcome to LUXURY RENTAL. Please read these terms and conditions
-          carefully before using our services.
+          Welcome to LUXURY VILLA RENTALS. Please read these terms and
+          conditions carefully before using our services.
         </p>
         <p>
           By accessing or using the Website, you agree to be bound by these
@@ -33,7 +33,7 @@ const TermsAndConditions = () => {
           <li>
             <h5>Use of the Website</h5>
             <ol>
-              <li> You must be at least [age] years old to use the Website.</li>
+              <li> You must be at least 18 years old to use the Website.</li>
               <li>
                 You agree to use the Website for lawful purposes and in
                 compliance with all applicable laws and regulations.
@@ -58,9 +58,9 @@ const TermsAndConditions = () => {
             <ol>
               <li>
                 The content on the Website, including but not limited to text,
-                graphics, logos, images, and software, is the property of [Your
-                Company] and is protected by copyright, trademark, and other
-                intellectual property laws.
+                graphics, logos, images, and software, is the property of LUXURY
+                VILLA RENTALS and is protected by copyright, trademark, and
+                other intellectual property laws.
               </li>
               <li>
                 You may not modify, publish, transmit, participate in the
@@ -73,8 +73,8 @@ const TermsAndConditions = () => {
           <li>
             <h5>Limitation of Liability</h5>
             <p>
-              To the fullest extent permitted by applicable law, [Your Company]
-              shall not be liable for any indirect, incidental, special,
+              To the fullest extent permitted by applicable law, LUXURY VILLA
+              RENTALS shall not be liable for any indirect, incidental, special,
               consequential, or punitive damages, or any loss of profits or
               revenues, whether incurred directly or indirectly, or any loss of
               data, use, goodwill, or other intangible losses, resulting from
@@ -92,7 +92,7 @@ const TermsAndConditions = () => {
               These Terms shall be governed by and construed in accordance with
               the laws of Jurisdiction. Contact Information If you have any
               questions about these Terms, please contact us at-
-              <span>luxuryvillarentals@gmail.com</span>.
+              <span>luxuryvillarentals3@gmail.com</span>.
             </p>
           </li>
         </ul>
