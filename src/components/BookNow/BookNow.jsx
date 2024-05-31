034c4eaece6abd0da927bc1241935f7b81733940
Vivek Awasthi <vivekawasthi055@gmail.com>
2024-05-31 11:34:58 +0530

    NEW CODE

--- src/components/BookNow/BookNow.jsx
+++ src/components/BookNow/BookNow.jsx
@@ -63,7 +63,7 @@ const BookNow = () => {
         To: userData.email,
         From: "luxuryvillarentals3@gmail.com",
         Subject: "Booking Confirmation",
-        Body: `<b>Dear ${fullName}</b>,<br>Your booking has been confirmed! We will contact you soon!<br><br><b>Your booking details entered by you are as follows :</b><br>Full Name: ${fullName}<br>Address: ${address}<br>Contact Number: ${phone}<br>E-mail: ${email}<br>Gender: ${gender}<br>Villa Category: ${category}<br>Villa Name & Location: ${selectedLocation}<br>Check-in Date: ${checkInDate}<br>Check-out Date: ${checkOutDate}<br><br><b>Thank you for choosing us.<br><br>Regards,<br>Team Luxury Villa Rentals</b>`,
+        Body: `<b>Dear ${fullName}</b>,<br>Your booking has been confirmed! We will contact you soon!<br><br><b>Your booking details entered by you are as follows :</b><br><br>Full Name: ${fullName}<br>Address: ${address}<br>Contact Number: ${phone}<br>E-mail: ${email}<br>Gender: ${gender}<br>Villa Category: ${category}<br>Villa Name & Location: ${selectedLocation}<br>Check-in Date: ${checkInDate}<br>Check-out Date: ${checkOutDate}<br><br><b>Thank you for choosing us.<br><br>Regards,<br>Team Luxury Villa Rentals</b>`,
       }).then(
         (message) => alert("Booking details have been sent to your email."),
         (error) => console.error("Error:", error)
